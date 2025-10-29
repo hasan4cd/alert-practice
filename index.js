039b55bebede6a8a0afae4502deec01c77fe0263
hasan4cd <hasan4cd@gmail.com>
2025-10-29 18:46:24 +0500

    Work Accomplish

---
+++ index.js
@@ -0,0 +1,29 @@
+// Question NO 1
+
+alert("Hello! This is my Landing Page");
+
+// Question NO 2
+
+alert("Error! Please enter a valid password.");
+
+// Question NO 3
+
+alert("Welcome to JS Land...\nHappy Coding!");
+
+// Question NO 4
+
+alert("Welcome to JS Land...");
+
+alert("Happy Coding!\nPrevent this page from creating additional dialogs");
+
+// Question NO 5
+
+alert("Hello... I can run JS through my web browser's console");
+
+// Question NO 6
+
+alert("Landing Page is fully Responsive");
+alert("I can create javascript built in function alert");
+alert("I can create images");
+alert("I can create styling through css");
+alert("I can create with my own idea");
